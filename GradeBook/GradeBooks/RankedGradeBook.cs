0bf71bb849c7ece326c0feb3c77175a9ef822b48
DakotaHutcheson <49033328+DakotaHutcheson@users.noreply.github.com>
2019-03-28 10:44:29 -0500

    Update RankedGradeBook.cs
    change

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -35,7 +35,7 @@ namespace GradeBook.GradeBooks
         public override void CalculateStatistics()
         {
             if (Students.Count < 5)
-                Console.WriteLine("Ranged grading requires at least 5 students with grades in order to properly calculate a student's overall grade");
+                throw new InvalidOperationException("Ranged grading requires at least 5 students with grades in order to properly calculate a student's overall grade");
             else
                 base.CalculateStatistics();
         }
